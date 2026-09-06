e428c207e4b9c3c8b747b9f4158d92c5d5c3f407
kevin20202003 <kchala900@gmail.com>
2026-09-06 13:09:32 -0500

    Implement two-factor authentication for user login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        if (app()->environment('production')) {
+            return;
+        }
+
         // User::factory(10)->create();
 
         User::factory()->create([
